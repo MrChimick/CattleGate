cce9d9c2a780bbef74e60ec19503a1654fe30b4c
Mr_Chimick <mikechimick@gmail.com>
2020-11-23 00:17:12 -0800

    Initial commit

---
+++ Building_CattleGate.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse;
+
+namespace CattleGate
+{
+    public class Building_CattleGate : Building_Door
+    {
+        public override bool PawnCanOpen(Pawn p)
+        {
+            return !p.AnimalOrWildMan();
+        }
+    }
+}
